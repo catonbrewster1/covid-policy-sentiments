7f849deee0f6138befa2e5b5bea6e9808f84eaab
Caton Brewster <catonbrewster@Catons-MacBook-Air-2020.local>
2021-12-10 13:26:35 -0600

    updated generating twitter data file

--- gen_data_extended.py
+++ gen_data_extended.py
@@ -21,7 +21,7 @@ SEARCH_TERMS = ["covid",
 QUERY = " OR ".join(SEARCH_TERMS)
 
 start_date = date(2021, 11, 30)
-stop_date = date(2021, 12, 10)
+stop_date = date(2021, 12, 5)
 
 def daterange(start_date, stop_date):
     for n in range(int((stop_date - start_date).days)):
@@ -31,58 +31,57 @@ def daterange(start_date, stop_date):
 dates =  list(daterange(start_date, stop_date))
 dates.reverse()
 
-def try_twint(c):
-    try: 
-        twint.run.Search(c)
-        df = twint.storage.panda.Tweets_df 
-        return False
-    except:
-        return True
-    
-error = True
-for end_date in dates:
-    print("Date: ", end_date)
-    c = twint.Config()
-    c.Search = QUERY
-    c.Lang = "es"
-    c.Geo = "19.4326, -99.1332, 20km" # Mexico City (radius is 5.5mi)
-    #c.Geo = "20.6597, -103.3496, 20km" # Guadalajara
-    c.Pandas = True
-    c.Limit = 1000
-    c.Until = end_date
-    c.Hide_output = True
-    df = None
-    while df is None:
-        try:
-            twint.run.Search(c)
-            df = twint.storage.panda.Tweets_df 
-        except:
-            pass
-    print(len(df))
-    if len(df) > 0:
-        df_dict = df.to_dict('index')
-        serialized_dict = pickle.dumps(df_dict)
-        file_name = end_date + ".json"
-        response = s3.put_object(Bucket=bucket_name, Key=file_name, Body=serialized_dict)
-        status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+geo_dict = {"CDMX": "19.4326, -99.1332, 20km", 
+            "GDL": "20.6597, -103.3496, 20km"}
 
-        if status == 200:
-            print(f"Successful S3 put_object response. Status - {status}")
-        else:
-            print(f"Unsuccessful S3 put_object response. Status - {status}")
-    else: 
-        print("didn't add")
-    time.sleep(10)
+# scrape 20 tweets each day from the last 10 days 
+# in Mexico City and Guadalajara
+for loc in geo_dict.keys():
+    for end_date in dates:
+        print("Date: ", end_date)
+        c = twint.Config()
+        c.Search = QUERY
+        c.Lang = "es"
+        c.Geo = geo_dict[loc]
+        c.Pandas = True
+        c.Limit = 20
+        c.Until = end_date
+        c.Hide_output = True
+        df = None
+        while df is None:
+            try:
+                twint.run.Search(c)
+                df = twint.storage.panda.Tweets_df 
+            except:
+                pass
+        print(len(df))
+        if len(df) > 0:
+            df_dict = df.to_dict('index')
+            serialized_dict = pickle.dumps(df_dict)
+            file_name = loc + "_" + end_date + ".json"
+            print(file_name)
+            response = s3.put_object(Bucket=bucket_name, Key=file_name, Body=serialized_dict)
+            status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+            if status == 200:
+                print(f"Successful S3 put_object response. Status - {status}")
+            else:
+                print(f"Unsuccessful S3 put_object response. Status - {status}")
+        else: 
+            print("didn't add")
+        time.sleep(10)
 
 
+'''
 for end_date in dates:
     obj = s3.get_object(Bucket=bucket_name, Key=end_date + ".json")
     obj_ser = obj["Body"].read()
     data = pickle.loads(obj_ser)
+    break
 
-'''
+
 #check objects in bucket
-for key in s3.list_objects(Bucket='lsc-tweets-finalproject')['Contents']:
+for key in s3.list_objects(Bucket=bucket_name)['Contents']:
     print(key['Key'])
 
 #empty & delete bucket
